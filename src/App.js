04023c2122bbebb51a0ab6a97476eaeddf211d11
Feng-Xu-aut <spz0564@autuni.ac.nz>
2022-04-03 16:46:17 +1200

    first message

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import {
+  Route,
+  NavLink,
+  Routes,
+  BrowserRouter as Router
+} from "react-router-dom";
+
+import Home from "./pages/Home";
+import SEPractice from "./pages/SE-Practice";
+import SubmitArticle from "./pages/Submit-Article"; 
+import NotFoundPage from "./pages/404";
+
+const App = () =>  {
+    return (
+        <Router>
+        <div>
+          <h1>Software Engineering Practice Evidence Repository (SEPER)</h1>
+            <ul className="header">
+                <li><NavLink exact to = "/">Home</NavLink></li>
+                <li><NavLink to = "/SEPractice">Select the Practice</NavLink></li>
+                <li><NavLink to = "/SubmitArticle">Submit an Article</NavLink></li>
+                <li><NavLink to = "/404">Not Found Page</NavLink></li>
+
+            </ul>
+          <div className="content">
+            <Routes>
+            <Route exact path="/" element={<Home />}/>
+            <Route  path="/SEPractice" element={<SEPractice />}/>
+            <Route  path="/SubmitArticle" element={<SubmitArticle />}/>
+            <Route exact path="/404"  element={<NotFoundPage />}/>
+            </Routes>
+          </div>
+        </div>
+        </Router>
+    );
+}
+ 
+export default App;
